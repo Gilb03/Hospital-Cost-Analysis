33fdcd823ce6d41d4536ae0d5a2c76d0cf834b01
Gilbert King <gilbertking91@gmail.com>
2020-12-01 11:54:29 -0600

    deployment init

--- app.R
+++ app.R
@@ -1,8 +1,9 @@
 library(shiny)
 library(dplyr)
 library(readr)
+library(rsconnect)
 library(shinydashboard)
-setwd("~/zrc/HHS_CostAnalysis/data")
+
 
 #LOAD DATA 
 hosp_cost <- read.csv("HospitalCosts.csv")
@@ -15,19 +16,12 @@ ui <- dashboardPage(
   dashboardSidebar(),
   dashboardBody(
     box(plotOutput("correlation_plot"), width=8),
-    box(
-      selectInput("Features", "Features:",
-                  c( "AGE")), width=4
-    ),
-    box(selectInput("Features", "Features:", 
-                    c("RACE")), width=4
-        )
+
   )
 )
 
 server <- function(input, output){
   output$correlation_plot <- renderPlot({
-  hist(hosp_cost$AGE, main="Cost Distribution by Age")
     hist(hosp_cost$RACE, main="Cost Distribution by Race")
     #hist(hosp_cost$FEMALE, main="Cost Distribution by Gender")
     
@@ -37,5 +31,6 @@ server <- function(input, output){
 }
 
 shinyApp(ui,server)
+
 
 
